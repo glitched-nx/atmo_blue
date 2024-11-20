a1d42871c689c9e1b520f571b5fcdbdf4b1e1257
glitched_nx <xteknox2009@gmail.com>
2024-11-20 23:01:39 +0100

    - Anpassungen für die Kompatibilität mit Systemversionen ab 19.0.0 - Korrektur der Debug-Fähigkeiten für Tinfoil und TinfoilForwarder - Überprüfung und Validierung der Acid-Signatur, damit die Anwendung nicht durch System-Updates blockiert wird.

--- stratosphere/loader/source/ldr_meta.cpp
+++ stratosphere/loader/source/ldr_meta.cpp
@@ -208,6 +208,20 @@ namespace ams::ldr {
         meta->acid->program_id_max = loc.program_id;
         meta->aci->program_id      = loc.program_id;
 
+        if (loc.program_id == ncm::TinfoilId::Tinfoil) {
+            if (hos::GetVersion() >= hos::Version_19_0_0) {
+                FixDebugCapabilityForHbl(static_cast<util::BitPack32 *>(meta->acid_kac), meta->acid->kac_size / sizeof(util::BitPack32));
+                FixDebugCapabilityForHbl(static_cast<util::BitPack32 *>(meta->aci_kac),  meta->aci->kac_size  / sizeof(util::BitPack32));
+            }
+        }
+
+        if (loc.program_id == ncm::TinfoilId::TinfoilForwarder) {
+            if (hos::GetVersion() >= hos::Version_19_0_0) {
+                FixDebugCapabilityForHbl(static_cast<util::BitPack32 *>(meta->acid_kac), meta->acid->kac_size / sizeof(util::BitPack32));
+                FixDebugCapabilityForHbl(static_cast<util::BitPack32 *>(meta->aci_kac),  meta->aci->kac_size  / sizeof(util::BitPack32));
+            }
+        }
+        
         /* For HBL, we need to copy some information from the base meta. */
         if (status.IsHbl()) {
             if (R_SUCCEEDED(fs::OpenFile(std::addressof(file), SdOrBaseMetaPath, fs::OpenMode_Read))) {
